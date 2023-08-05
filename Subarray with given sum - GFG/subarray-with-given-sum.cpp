97a4b29c561323cb16e1af1afd971aad608d60cc
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-08-05 21:01:47 +0530

    Added solution - LeetHub

---
+++ Subarray with given sum - GFG/subarray-with-given-sum.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int>arr, int n, long long s)
+    {
+        // Your code here
+        vector<int>ans;
+        long long sum=0;
+        int start=0;
+        for(int end=0;end<n;end++){
+            sum+=arr[end];
+            while(sum>s && start<end){
+                sum-=arr[start];
+                start++;
+            }
+            if(sum==s){
+                ans.push_back(start+1);
+                ans.push_back(end+1);
+                return ans;
+            }
+        }
+        return {-1};
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        vector<int>arr(n);
+        // int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
