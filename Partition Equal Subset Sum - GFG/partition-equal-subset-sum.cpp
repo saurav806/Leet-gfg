165da0502b7a0e0fd21f3d88c1cdc060260dbf98
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-08-06 15:06:54 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -9,15 +9,15 @@ using namespace std;
 
 class Solution{
 public:
-    int partition(int n, int arr[], int sum){
-        if(sum==0) return true;
-        if(n==0) return false;
+    // int partition(int n, int arr[], int sum){
+    //     if(sum==0) return true;
+    //     if(n==0) return false;
         
-        if(arr[n-1]<=sum)
-            return partition(n-1,arr,sum-arr[n-1]) || partition(n-1,arr,sum);
-        else
-            return partition(n-1,arr,sum);
-    }
+    //     if(arr[n-1]<=sum)
+    //         return partition(n-1,arr,sum-arr[n-1]) || partition(n-1,arr,sum);
+    //     else
+    //         return partition(n-1,arr,sum);
+    // }
     
     int equalPartition(int N, int arr[])
     {
@@ -26,8 +26,25 @@ public:
         for(int i=0;i<N;i++)
             sum+=arr[i];
         if(sum%2!=0) return 0;
+        int m=sum/2;
+        int bag[N+1][m+1];
+        for(int i=0;i<=N;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0) bag[i][j] = 0;
+                if(j==0) bag[i][j] = 1;
+            }
+        }
         
-        return partition(N, arr, sum/2);
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=m;j++){
+                if(arr[i-1] <= j)
+                    bag[i][j] = bag[i-1][j-arr[i-1]] || bag[i-1][j];
+                else
+                    bag[i][j] = bag[i-1][j];
+            }
+        }
+        
+        return bag[N][m];
     }
 };
 
