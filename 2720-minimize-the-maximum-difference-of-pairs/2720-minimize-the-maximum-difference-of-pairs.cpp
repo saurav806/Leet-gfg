9d75df50a813aeb761e681d11062a76432189f2b
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-08-09 23:41:29 +0530

    Time: 124 ms (96.96%), Space: 81.2 MB (38.62%) - LeetHub

--- 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.cpp
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.cpp
@@ -1,33 +1,29 @@
 class Solution {
-public:
-    int minimizeMax(std::vector<int>& nums, int p) {
-        std::sort(nums.begin(), nums.end());
-        
-        int left = 0, right = nums.back() - nums.front();
-        
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (can_form_pairs(nums, mid, p)) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        
-        return left;
-    }
-    
 private:
-    bool can_form_pairs(const std::vector<int>& nums, int mid, int p) {
-        int count = 0;
-        for (int i = 0; i < nums.size() - 1 && count < p;) {
-            if (nums[i+1] - nums[i] <= mid) {
+    bool isValid(vector<int>& nums, int mid, int p){
+        int count=0;
+        for(int i=0;i<nums.size()-1 && count<p;){
+            if(nums[i+1] - nums[i] <= mid){
                 count++;
                 i += 2;
-            } else {
-                i++;
             }
+            else
+                i++;
         }
         return count >= p;
+    }
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(), nums.end());
+        int l=0;
+        int r= nums.back() - nums.front();
+        while(l<r){
+            int mid= l+(r-l)/2;
+            if( isValid(nums, mid, p))
+                r = mid;
+            else
+                l= mid+1;
+        }
+        return l;
     }
 };
