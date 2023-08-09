94d29da871783c2701d3017dff0257d1e455329b
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-08-09 22:29:37 +0530

    Time: 130 ms (6.54%), Space: 154.3 MB (5.04%) - LeetHub

---
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mcm(vector<int>arr, int i, int j, vector<vector<int>>&dp){
+        if(i>=j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int ans= INT_MAX;
+        for(int k=i;k<=j-1;k++){
+            int temp = mcm(arr, i, k, dp) + mcm(arr, k+1,j, dp) + (arr[i-1] * arr[j] * arr[k]);
+            
+            if(temp<ans)
+                ans= temp;
+        }
+        return dp[i][j] = ans;
+    }
+
+    int minScoreTriangulation(vector<int>& values) {
+        int N= values.size();
+        vector<vector<int>>dp(N+1, vector<int>(N+1, -1));
+        int ans= mcm(values, 1, N-1,dp);
+        return ans;
+    }
+};
