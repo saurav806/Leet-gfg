d926a758b486f15e683197107421e30582e0d1eb
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-08-13 14:58:07 +0530

    Time: 0 ms (100%), Space: 14 MB (63.76%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
-        
-        while(l <= r)
-        {
-            int mid = l + (r-l) / 2;
-            if (nums[mid] == target)
-                return true;
-			// with duplicates we can have this contdition, just update left & right
-            if((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
-            {
-                l++;
-                r--;
+        int start =0;
+        int end= nums.size()-1;
+        while(start<=end){
+            int mid= start+ (end-start)/2;
+            if(nums[mid] == target) return true;
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]){
+                start++;
+                end--;
+                continue;
             }
-			// first half
-			// first half is in order
-            else if(nums[l] <= nums[mid])
-            {
-				// target is in first  half
-                if((nums[l] <= target) && (nums[mid] > target))
-                    r = mid - 1;
+            if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target <= nums[mid])
+                    end = mid-1;
                 else
-                    l = mid + 1;
+                    start = mid+1;
             }
-			// second half
-			// second half is order
-			// target is in second half
-            else
-            {
-                if((nums[mid] < target) && (nums[r]>= target))
-                    l = mid + 1;
+            else{
+                if(nums[mid] <= target && target <= nums[end])
+                    start = mid+1;
                 else
-                    r = mid - 1;
+                    end = mid-1;
             }
         }
         return false;
