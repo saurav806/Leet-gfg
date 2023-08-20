bff96d6960c9227871dd2070e1db37d2c137695f
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-08-20 12:30:51 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,42 +10,43 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-	int foc(int arr[], int n, int x){
-	    int start = 0;
-	    int end = n-1;
-	    int ind=n-1;
-	    while(start<=end){
-	        int mid = start+ (end-start)/2;
-	        if(arr[mid] == x){
-	            ind = min(ind, mid);
-	            end = mid-1;
-	        }
-	        else if(arr[mid] < x) start = mid+1;
-	        else end = mid-1;
-	    }
-	    return ind;
-	}
-	int loc(int arr[], int n, int x){
-	    int start = 0;
-	    int end = n-1;
-	    int ind = -1;
-	    while(start<=end){
-	        int mid = start + (end-start)/2;
-	        if(arr[mid] == x){
-	            ind = max(ind, mid);
-	            start = mid+1;
-	        }
-	        else if(arr[mid] < x) start = mid +1 ;
-	        else end = mid - 1;
-	    }
-	    return ind;
-	}
+// 	int foc(int arr[], int n, int x){
+// 	    int start = 0;
+// 	    int end = n-1;
+// 	    int ind=n-1;
+// 	    while(start<=end){
+// 	        int mid = start+ (end-start)/2;
+// 	        if(arr[mid] == x){
+// 	            ind = min(ind, mid);
+// 	            end = mid-1;
+// 	        }
+// 	        else if(arr[mid] < x) start = mid+1;
+// 	        else end = mid-1;
+// 	    }
+// 	    return ind;
+// 	}
+// 	int loc(int arr[], int n, int x){
+// 	    int start = 0;
+// 	    int end = n-1;
+// 	    int ind = -1;
+// 	    while(start<=end){
+// 	        int mid = start + (end-start)/2;
+// 	        if(arr[mid] == x){
+// 	            ind = max(ind, mid);
+// 	            start = mid+1;
+// 	        }
+// 	        else if(arr[mid] < x) start = mid +1 ;
+// 	        else end = mid - 1;
+// 	    }
+// 	    return ind;
+// 	}
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int low = foc(arr,n,x);
-	    int high = loc(arr,n,x);
-	    if(high == -1) return 0;
-	    return high-low+1;
+	   // int low = foc(arr,n,x);
+	   // int high = loc(arr,n,x);
+	   // if(high == -1) return 0;
+	   // return high-low+1;
+	   return upper_bound(arr,arr+n,x) - lower_bound(arr,arr+n,x);
 	}
 };
 
