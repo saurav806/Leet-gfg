530ea6e466e378a97a4b8ec7de8cc51be7815ec8
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-09-05 15:43:33 +0530

    Time: 100 ms (56.5%), Space: 93.3 MB (88.19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,8 +4,8 @@ public:
         int ans = 0;
         int mini = prices[0];
         for(int i=1;i<prices.size();i++){
-            mini = min(mini,prices[i]);
             ans = max(prices[i]-mini,ans);
+            mini = min(mini,prices[i]);
         }
         return ans;
     }
