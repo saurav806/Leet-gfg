0f048a5b8575bf56dd331c06927bdd22e63ce9ed
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-09-23 21:32:51 +0530

    Added solution - LeetHub

---
+++ Equilibrium Point - GFG/equilibrium-point.js
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let a = new Array(n);
+        let input_ar = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++)
+            a[i] = input_ar[i];
+        let obj = new Solution();
+        console.log(obj.equilibriumPoint(a, n));
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} a
+ * @param {number} n
+ * @returns {number}
+*/
+
+class Solution {
+    // Function to find equilibrium point in the array.
+    equilibriumPoint(a, n)
+    {
+        //your code here
+        let sum = 0;
+        for(let i = 0; i<n ;i++){
+            sum = sum + a[i];
+        }
+        let l_sum = 0;
+        for(let i = 0; i<n ;i++){
+            sum= sum -a[i];
+            if(sum == l_sum) return i+1;
+            l_sum = l_sum+a[i];
+            
+        }
+        return -1;
+    }
+}
