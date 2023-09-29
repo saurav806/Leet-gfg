5dab7065e7701c2e6449a48352b9f0a40e76e0ed
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-09-29 22:03:08 +0530

    Time: 137 ms (14.11%), Space: 96.8 MB (13.85%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -3,20 +3,6 @@ public:
     bool isMonotonic(vector<int>& nums) {
         int n=nums.size();
         if(n==1 || n==2) return true;
-        // if(nums[0] > nums[1]){
-        //     int i=1;
-        //     while(--n > 0){
-        //         if(nums[i] > nums[i-1]) return false;
-        //         i++;
-        //     }
-        // }
-        // else{
-        //     int i=1;
-        //     while(--n > 0){
-        //         if(nums[i] < nums[i-1]) return false;
-        //         i++;
-        //     }
-        // }
         int dir = 0;
         for(int i=1;i<n;i++){
             if(nums[i] > nums[i-1]){
