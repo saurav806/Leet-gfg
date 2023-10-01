0cad24e27e5cd49859e68fd8097d62970b9994e2
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-10-01 22:47:39 +0530

    Time: 16 ms (55.57%), Space: 11.2 MB (43.02%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -5,12 +5,11 @@ public:
         string temp = "";
         for(int i=0;i<s.size();i++){
             int end=i-1;
-            
-            if(i==s.size()-1 || s[i] == ' '){
-                if(i==s.size()-1){
+            if(i==s.size()-1){
                 temp+=s[i];
                 end=i;
             }
+            if(i==s.size()-1 || s[i] == ' '){
                 while(start<end){
                     char ch = temp[start];
                     temp[start] = temp[end];
